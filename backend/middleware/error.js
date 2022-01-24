2d40a8b854cafe5c4e419953f49a7a10eb7e1e15
Biniam Daniel <biniamdnl@gmail.com>
2022-01-24 16:17:51 +0300

    error handling done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,7 +3,16 @@ const ErrorHandler = require("../utils/errorhandler");
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 5000;
-    err.message = err.message || "Internal server error"
+    err.message = err.message || "Internal server error";
+
+
+    if (err.name === "CastError"){
+        const message = `Resource not found invalid ${err.path}`;
+        err = new ErrorHandler(message, 404)
+    }
+
+
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
